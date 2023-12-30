9f3769d3c0aec6b1319eb22f877bfe050f98c139
Andres Gaibor <andresalexander14@hotmail.com>
2023-12-29 20:17:53 -0500

    correccion de creacion de archivos

--- WinAppDiseños/Archivos.cs
+++ WinAppDiseños/Archivos.cs
@@ -20,12 +20,10 @@ namespace WinAppDiseños
                 // Verifica si el archivo ya existe
                 if (!File.Exists(nombre))
                 {
-                    // El archivo no existe, así que créalo
-                    using (FileStream fs = File.Create(nombre))
-                    {
+                    
                         // Puedes agregar contenido al archivo aquí si lo deseas
                         File.Create(nombre).Close();
-                    }
+                    
                 }
                 
             }
