272bd578070c8548dbe921fbe6e4aef4450eee08
Andres Gaibor <andresalexander14@hotmail.com>
2024-01-01 23:54:48 -0500

    reporte de ventas por fecha

--- WinAppDiseños/ReporteVentasFecha.Designer.cs
+++ WinAppDiseños/ReporteVentasFecha.Designer.cs
@@ -29,73 +29,35 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource5 = new Microsoft.Reporting.WinForms.ReportDataSource();
-            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource6 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource3 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource4 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.facturaDetalleFactBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.facturaBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dataSet1 = new WinAppDiseños.DataSet1();
+            this.clienteBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.facturaBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.dataSet1 = new WinAppDiseños.DataSet1();
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
             this.label2 = new System.Windows.Forms.Label();
             this.pictureBox6 = new System.Windows.Forms.PictureBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.facturaDetalleFactBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.clienteBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.pictureBox7 = new System.Windows.Forms.PictureBox();
-            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.facturaDetalleFactBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.facturaBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
-            this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.facturaDetalleFactBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.clienteBindingSource)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
             this.SuspendLayout();
             // 
-            // reportViewer1
+            // facturaDetalleFactBindingSource
             // 
-            this.reportViewer1.AutoSize = true;
-            reportDataSource5.Name = "DataSet1";
-            reportDataSource5.Value = this.facturaDetalleFactBindingSource;
-            reportDataSource6.Name = "DataSet2";
-            reportDataSource6.Value = this.clienteBindingSource;
-            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource5);
-            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource6);
-            this.reportViewer1.LocalReport.ReportEmbeddedResource = "WinAppDiseños.Reportes.Inf_Ventas_Fecha.rdlc";
-            this.reportViewer1.Location = new System.Drawing.Point(266, 62);
-            this.reportViewer1.Name = "reportViewer1";
-            this.reportViewer1.PromptAreaCollapsed = true;
-            this.reportViewer1.ServerReport.BearerToken = null;
-            this.reportViewer1.Size = new System.Drawing.Size(583, 459);
-            this.reportViewer1.TabIndex = 0;
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.dateTimePicker1);
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(2, 62);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(258, 216);
-            this.groupBox1.TabIndex = 1;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Parametros";
-            // 
-            // dateTimePicker1
-            // 
-            this.dateTimePicker1.DataBindings.Add(new System.Windows.Forms.Binding("Value", this.facturaBindingSource, "fechacompra", true, System.Windows.Forms.DataSourceUpdateMode.OnValidation, null, "d"));
-            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateTimePicker1.Location = new System.Drawing.Point(35, 74);
-            this.dateTimePicker1.MaxDate = new System.DateTime(2024, 1, 2, 0, 0, 0, 0);
-            this.dateTimePicker1.MinDate = new System.DateTime(2020, 1, 25, 0, 0, 0, 0);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(111, 23);
-            this.dateTimePicker1.TabIndex = 1;
-            this.dateTimePicker1.Value = new System.DateTime(2024, 1, 1, 0, 0, 0, 0);
-            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            this.facturaDetalleFactBindingSource.DataMember = "Factura_DetalleFact";
+            this.facturaDetalleFactBindingSource.DataSource = this.facturaBindingSource;
             // 
             // facturaBindingSource
             // 
@@ -107,12 +69,66 @@
             this.dataSet1.DataSetName = "DataSet1";
             this.dataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
+            // clienteBindingSource
+            // 
+            this.clienteBindingSource.DataMember = "Cliente";
+            this.clienteBindingSource.DataSource = this.dataSet1;
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.AutoSize = true;
+            reportDataSource3.Name = "DataSet1";
+            reportDataSource3.Value = this.facturaDetalleFactBindingSource;
+            reportDataSource4.Name = "DataSet2";
+            reportDataSource4.Value = this.clienteBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource3);
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource4);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "WinAppDiseños.Reportes.Inf_Ventas_Fecha.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(532, 119);
+            this.reportViewer1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.PromptAreaCollapsed = true;
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(1164, 881);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(4, 119);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.groupBox1.Size = new System.Drawing.Size(516, 415);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Parametros";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.DataBindings.Add(new System.Windows.Forms.Binding("Value", this.facturaBindingSource, "fechacompra", true, System.Windows.Forms.DataSourceUpdateMode.OnValidation, null, "d"));
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(70, 142);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.dateTimePicker1.MaxDate = new System.DateTime(2024, 1, 2, 0, 0, 0, 0);
+            this.dateTimePicker1.MinDate = new System.DateTime(2020, 1, 25, 0, 0, 0, 0);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(218, 38);
+            this.dateTimePicker1.TabIndex = 1;
+            this.dateTimePicker1.Value = new System.DateTime(2024, 1, 1, 0, 0, 0, 0);
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(10, 39);
+            this.label1.Location = new System.Drawing.Point(20, 75);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(151, 17);
+            this.label1.Size = new System.Drawing.Size(270, 31);
             this.label1.TabIndex = 0;
             this.label1.Text = "Fecha de busqueda";
             // 
@@ -125,26 +141,42 @@
             this.panel1.Controls.Add(this.label3);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(849, 59);
+            this.panel1.Size = new System.Drawing.Size(1698, 113);
             this.panel1.TabIndex = 12;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox7.Image = global::WinAppDiseños.Properties.Resources.retroceder;
+            this.pictureBox7.Location = new System.Drawing.Point(30, 13);
+            this.pictureBox7.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(100, 90);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 33;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Click += new System.EventHandler(this.pictureBox7_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(116, 17);
+            this.label2.Location = new System.Drawing.Point(232, 33);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(323, 31);
+            this.label2.Size = new System.Drawing.Size(639, 63);
             this.label2.TabIndex = 13;
             this.label2.Text = "Reporte Ventas por fecha";
             // 
             // pictureBox6
             // 
             this.pictureBox6.Image = global::WinAppDiseños.Properties.Resources.Logo;
-            this.pictureBox6.Location = new System.Drawing.Point(646, 7);
+            this.pictureBox6.Location = new System.Drawing.Point(1292, 13);
+            this.pictureBox6.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(51, 49);
+            this.pictureBox6.Size = new System.Drawing.Size(102, 94);
             this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox6.TabIndex = 10;
             this.pictureBox6.TabStop = false;
@@ -153,55 +185,35 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(32, 20);
+            this.label3.Location = new System.Drawing.Point(64, 38);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(0, 29);
+            this.label3.Size = new System.Drawing.Size(0, 55);
             this.label3.TabIndex = 2;
-            // 
-            // facturaDetalleFactBindingSource
-            // 
-            this.facturaDetalleFactBindingSource.DataMember = "Factura_DetalleFact";
-            this.facturaDetalleFactBindingSource.DataSource = this.facturaBindingSource;
-            // 
-            // clienteBindingSource
-            // 
-            this.clienteBindingSource.DataMember = "Cliente";
-            this.clienteBindingSource.DataSource = this.dataSet1;
-            // 
-            // pictureBox7
-            // 
-            this.pictureBox7.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.pictureBox7.Image = global::WinAppDiseños.Properties.Resources.retroceder;
-            this.pictureBox7.Location = new System.Drawing.Point(15, 7);
-            this.pictureBox7.Name = "pictureBox7";
-            this.pictureBox7.Size = new System.Drawing.Size(50, 47);
-            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox7.TabIndex = 33;
-            this.pictureBox7.TabStop = false;
-            this.pictureBox7.Click += new System.EventHandler(this.pictureBox7_Click);
             // 
             // ReporteVentasFecha
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(849, 523);
+            this.ClientSize = new System.Drawing.Size(1698, 1006);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.reportViewer1);
-            this.MinimumSize = new System.Drawing.Size(816, 562);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.MinimumSize = new System.Drawing.Size(1606, 1015);
             this.Name = "ReporteVentasFecha";
             this.Text = "Reporte de Ventas por Fecha";
             this.Load += new System.EventHandler(this.ReporteVentasFecha_Load);
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.facturaDetalleFactBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.facturaBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.clienteBindingSource)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.facturaDetalleFactBindingSource)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.clienteBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- WinAppDiseños/ReporteVentasFecha.cs
+++ WinAppDiseños/ReporteVentasFecha.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +21,7 @@ namespace WinAppDiseños
 
         private void ReporteVentasFecha_Load(object sender, EventArgs e)
         {
+            this.dataSet1.leerXml();
 
             this.reportViewer1.RefreshReport();
         }
@@ -30,7 +32,9 @@ namespace WinAppDiseños
             DateTime fechaSeleccionada = dateTimePicker1.Value;
 
             // Llama a un método que carga y muestra el informe con la fecha seleccionada
-            CargarInforme(fechaSeleccionada);
+            //CargarInforme(fechaSeleccionada);
+
+            MostrarInformeConFiltro(fechaSeleccionada);
         }
 
         private void CargarInforme(DateTime fecha)
@@ -46,9 +50,80 @@ namespace WinAppDiseños
             // Actualiza y muestra el informe
             reportViewer1.RefreshReport();
         }
+
+        private void MostrarInformeConFiltro(DateTime fechacompra)
+        {
+            if (true)
+            {
+                // Get a filtered dataset based on the selected cedula
+                DataSet filteredDataSet = GetFilteredDataSet(fechacompra);
+
+                // Configure the ReportViewer
+                reportViewer1.Reset();
+                reportViewer1.LocalReport.ReportPath = Path.Combine(Archivos.rutaDebug, "../../Reportes/Inf_Ventas_Fecha.rdlc");
+
+
+                // Set the data source for the report to the filtered dataset
+                if (filteredDataSet.Tables.Count >= 2)
+                {
+                    // Set the data source for the report to the filtered datasets
+                    ReportDataSource rds1 = new ReportDataSource("DataSet1", filteredDataSet.Tables["Factura"]);
+                    ReportDataSource rds2 = new ReportDataSource("DataSet2", filteredDataSet.Tables["Cliente"]);
+                    reportViewer1.LocalReport.DataSources.Clear();
+                    reportViewer1.LocalReport.DataSources.Add(rds1);
+                    reportViewer1.LocalReport.DataSources.Add(rds2);
+
+                    // Refresh the report
+                    reportViewer1.RefreshReport();
+                }
+                else
+                {
+                    MessageBox.Show("The filtered dataset does not contain the expected number of tables.", "Data Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+
+            }
+
+        }
+
+        private DataSet GetFilteredDataSet(DateTime fechacompra)
+        {
+            // Create a new DataSet to hold the filtered data
+            DataSet filteredDataSet = new DataSet();
+
+            try
+            {
+                // Filter the Cliente data
+                DataTable originalTable1 = dataSet1.Tables["Cliente"];
+                //DataView view1 = new DataView(originalTable1);
+                //view1.RowFilter = $"cedula = '{cedula}'";  // Adjust the filter to match your dataset's structure
+                //DataTable filteredTable1 = view1.ToTable();
+                filteredDataSet.Tables.Add(originalTable1);
+
+                // Filter the Factura data
+                DataTable originalTable2 = dataSet1.Tables["Factura"];
+                DataView view2 = new DataView(originalTable2);
+                
+                view2.RowFilter = $"fechacompra = {fechacompra}";  // Adjust the filter to match your dataset's structure
+                DataTable filteredTable2 = view2.ToTable();
+                filteredDataSet.Tables.Add(filteredTable2);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error filtering dataset: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            return filteredDataSet;
+        }
+
         private void pictureBox7_Click(object sender, EventArgs e)
         {
             Close();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
         }
     }
 }
