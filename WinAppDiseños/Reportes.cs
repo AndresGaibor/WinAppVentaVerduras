20fc773793beb092da793529de26a3a36c31c13a
Erick Joel Malan <erickjpk1234@gmail.com>
2024-01-02 22:51:44 -0500

    Esta si vale

--- WinAppDiseños/Reportes.cs
+++ WinAppDiseños/Reportes.cs
@@ -16,7 +16,7 @@ namespace WinAppDiseños
         {
             InitializeComponent();
         }
-
+        //JIJIJIA
         private void pictureBox2_Click(object sender, EventArgs e)
         {
             Rpt_Verduras rpt_Verduras = new Rpt_Verduras();
